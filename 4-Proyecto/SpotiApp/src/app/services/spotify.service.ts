74b7eac710bb05e0594a1cec81d339ca8883f440
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-17 21:07:10 -0500

    Imágenes de funcionalidad

--- 4-Proyecto/SpotiApp/src/app/services/spotify.service.ts
+++ 4-Proyecto/SpotiApp/src/app/services/spotify.service.ts
@@ -16,7 +16,7 @@ export class SpotifyService {
     const URL = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAbHH8_S5DZpDGWNeUWfoyD2h76CVNaYNW0GXO106eOvYzOuZbRHozffEnQSx-5OpElOJVWPR3-hVwd8jM'
+      'Authorization': 'Bearer BQCA2g0td1laym1L8p0Y9Pu8k1-JLiQHqgb2Hl1FiT34d8osRZws5TE8-fWp2baE3J3DCaDvU0ewe6I2Yhc'
     });
 
     return this.httpClient.get(URL, { headers });
