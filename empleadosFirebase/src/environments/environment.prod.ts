53d54358312fb2b5419e52e5595160f17b810fcd
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-12-16 20:03:37 -0500

    Adicion de Firebase

--- empleadosFirebase/src/environments/environment.prod.ts
+++ empleadosFirebase/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAbPj-aS1jUSESCMxlsBYdMfSs73sYyNgI",
+    authDomain: "pruebasfirebase-d415f.firebaseapp.com",
+    projectId: "pruebasfirebase-d415f",
+    storageBucket: "pruebasfirebase-d415f.appspot.com",
+    messagingSenderId: "35588908197",
+    appId: "1:35588908197:web:64716517829056cd79594c"
+  }
 };
--- empleadosFirebase/src/environments/environment.ts
+++ empleadosFirebase/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAbPj-aS1jUSESCMxlsBYdMfSs73sYyNgI",
+    authDomain: "pruebasfirebase-d415f.firebaseapp.com",
+    projectId: "pruebasfirebase-d415f",
+    storageBucket: "pruebasfirebase-d415f.appspot.com",
+    messagingSenderId: "35588908197",
+    appId: "1:35588908197:web:64716517829056cd79594c"
+  }
 };
 
 /*
