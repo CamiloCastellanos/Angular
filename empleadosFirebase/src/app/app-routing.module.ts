88670cf7999687f60603110a286bfd84d45642e6
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-12-16 20:02:56 -0500

    Creacion de rutas

--- empleadosFirebase/src/app/app-routing.module.ts
+++ empleadosFirebase/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "listaEmpleados", component: ListEmpleadosComponent },
+  { path: "crearEmpleado", component: CreateEmpleadoComponent },
+  { path: "", redirectTo: "listaEmpleados", pathMatch: "full" },
+  { path: "**", redirectTo: "listaEmpleados", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
