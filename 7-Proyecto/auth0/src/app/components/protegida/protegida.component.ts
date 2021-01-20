b74007dbb93fab8bd29830c054cc07878bdf897c
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-01-20 16:12:22 -0500

    Se modifica la Interfaz para mostrar los datos recopilados des Login realizado por el usuario.

--- 7-Proyecto/auth0/src/app/components/protegida/protegida.component.ts
+++ 7-Proyecto/auth0/src/app/components/protegida/protegida.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-protegida',
   templateUrl: './protegida.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtegidaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) {}
 
   ngOnInit(): void {
   }
